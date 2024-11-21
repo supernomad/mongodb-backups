d33a31322d48149181c97a783b89294eee640824
Christian Saide <me@csaide.dev>
2024-11-20 23:31:44 -0500

    Using URI instead of host in mongodump command.

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ TARGET_FILENAME=mongo-$MONGO_DB-$DATE_STR
 TARGET_PATH=/workdir/data/$TARGET_FILENAME
 
 echo $DATE_STR > /workdir/data/backup.date
-mongodump -h $MONGO_HOST -d $MONGO_DB -o $TARGET_PATH
+mongodump --uri="$MONGO_HOST" -d $MONGO_DB -o $TARGET_PATH
 tar cfzv $TARGET_PATH.tar.gz $TARGET_PATH
 rm -rf $TARGET_PATH
 gsutil cp $TARGET_PATH.tar.gz gs://$BACKUPS_GS_BUCKET/$TARGET_FILENAME.tar.gz
